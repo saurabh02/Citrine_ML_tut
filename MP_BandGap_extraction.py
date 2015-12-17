f657a4ddbdabe8dffc9e67947b5b37758afaa965
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2015-12-17 14:35:43 -0800

    Extract HF and density, dump into csv

--- MP_BandGap_extraction.py
+++ MP_BandGap_extraction.py
@@ -1,14 +1,27 @@
 from pymatgen import MPRester, periodic_table
 import itertools
+import csv
 
-#mpr = MPRester(MAPI_KEY) # You have to register with Materials Project to receive an API
+# mpr = MPRester(MAPI_KEY) # You have to register with Materials Project to receive an API
 
 # There are 103 elements in pymatgen's list, giving C(103, 2) = 5253 binary systems
-allBinaries = itertools.combinations(periodic_table.all_symbols(), 2) # Create list of all binary systems
+allBinaries = itertools.combinations(periodic_table.all_symbols(), 2)  # Create list of all binary systems
 
 with MPRester() as m:
-    for system in allBinaries:
-        results = m.get_data(system[0] + '-' + system[1], data_type='vasp') # Download DFT data for each binary system
-        for material in results: # We will receive many compounds within each binary system
-            if material['e_above_hull'] < 1e-6: # Check if this compound is thermodynamically stable
-                print(material['pretty_formula'] + ',' + str(material['band_gap'])) # Output band gap csv to the screen
+    with open('bandgap_energy_densityDFT.csv', 'wb') as csvfile:
+        csv_writer = csv.writer(csvfile,delimiter=',')
+        for system in allBinaries:
+            results = m.get_data(system[0] + '-' + system[1],
+                                 data_type='vasp')  # Download DFT data for each binary system
+            # print results
+            for material in results:  # We will receive many compounds within each binary system
+                if material['e_above_hull'] < 1e-6:  # Check if this compound is thermodynamically stable
+                    dat = []
+                    dat.append(material['pretty_formula'])
+                    dat.append(str(material['band_gap']))
+                    dat.append(str(material['formation_energy_per_atom']))
+                    dat.append(str(material['density']))  # Output band gap csv to the screen
+                    csv_writer.writerow(dat)
+
+
+csvfile.close()
